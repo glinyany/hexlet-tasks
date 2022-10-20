e1b32b6bd717e98812c0ef759ed4bd34dbafe4f0
vladyslav <glinyany.lie@gmail.com>
2022-10-20 23:19:49 +0200

    fix

--- redux toolkit/3. Подробнее о работе слайсов/components/Comment.jsx
+++ redux toolkit/3. Подробнее о работе слайсов/components/Comment.jsx
@@ -1,4 +1,36 @@
-  // BEGIN
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Comment = ({ commentId }) => {
+  // BEGIN (write your solution here)
+  console.log('inside COMMENT: ', commentId);
+  const comment = useSelector((state) => {
+    const currentComment = state.commentsReducer.comments.filter((com) => com.id === commentId);
+    return currentComment[0];
+  });
+
+  const author = useSelector((state) => {
+    const currentAuthor = state.usersReducer.users.filter((user) => user.username === comment.author);
+    return currentAuthor[0];
+  });
+  // END
+
+  if (!author || !comment) {
+    return null;
+  }
+
+  return (
+    <>
+      <h5 className="card-title">{ author.name }</h5>
+      <p className="card-text">{ comment.text }</p>
+    </>
+  );
+};
+
+export default Comment;
+
+  /* BEGIN
   // для получения данных из разных селекторов лучше всего использовать `createSelector`,
   // но для решения этого задания можно обычные селекторы
   const { author, comment } = useSelector((state) => {
@@ -10,4 +42,4 @@
     const currentAuthor = state.usersReducer.users.find(({ id }) => id === currentComment.author);
     return { author: currentAuthor, comment: currentComment };
   });
-  // END
+  */
